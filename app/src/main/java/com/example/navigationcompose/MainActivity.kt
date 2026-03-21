195e7f13e40402cbc59d4c1c2494996f25ff7516
Nicole Alves Nogueira <nicolefaculdade2024@gmail.com>
2026-03-20 23:38:06 -0300

    Remove exemplos e limpa MainActivity

--- app/src/main/java/com/example/navigationcompose/MainActivity.kt
+++ app/src/main/java/com/example/navigationcompose/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.navigationcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
+
+            }
         }
     }
-}
