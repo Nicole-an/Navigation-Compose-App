3f2a8c0580dd1de5a31ba30ece311aeb2255f94e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-24 20:45:28 -0300

    checkpoint1

---
+++ app/src/main/java/com/example/navigationcompose/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.navigationcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.navigationcompose.ui.theme.NavigationComposeAppTheme
+import com.example.navigationcompose.ui.theme.screens.LoginScreen
+import com.example.navigationcompose.ui.theme.screens.MenuScreen
+import com.example.navigationcompose.ui.theme.screens.PedidosScreen
+import com.example.navigationcompose.ui.theme.screens.PerfilScreen
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Android5navigationbetweenscreensTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "login",
+                    ) {
+                        composable(route = "login") {
+                            LoginScreen(
+                                modifier = Modifier.padding(innerPadding),
+                                navController
+                            )
+                        }
+                        composable(route = "menu") {
+                            MenuScreen(
+                                modifier = Modifier.padding(innerPadding),
+                                navController
+                            )
+                        }
+                        composable(
+                            route = "pedidos?cliente={cliente}",
+                            arguments = listOf(navArgument("cliente") {
+                                defaultValue = "Cliente Nicole"
+                            })
+                        ) {
+                            PedidosScreen(
+                                modifier = Modifier.padding(innerPadding),
+                                navController,
+                                it.arguments?.getString("cliente")
+                            )
+                        }
+                    }
+                    composable(
+                        route = "perfil/{nome}/{idade}",
+                        arguments = listOf(
+                            navArgument("nome") { type = NavType.StringType },
+                            navArgument("idade") { type = NavType.IntType }
+                        )
+                    ) {
+                        val nome: String? = it.arguments?.getString("nome", "Usuário Nicole")
+                        val idade: Int? = it.arguments?.getInt("idade", 20)
+                        PerfilScreen(
+                            modifier = Modifier.padding(innerPadding),
+                            navController,
+                            nome!!,
+                            idade!!
+                    }
+                    )
+                }
+            }
+        }
+    }
+}
+}
+}
